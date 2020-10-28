4a719356cf9b41052e575cb113c0488f98e08169
root <root@server73.ukr-mobil.com>
2020-10-28 05:35:24 -0700

    remove unused files

--- ego/views/cache/0c06abea278387b9a5823ffea3f4e74a415613f3.php
+++
@@ -1,8 +0,0 @@
-<?php echo $__env->make('mails.header', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-
-<div class="message">
-    <p style="<?php echo e($email->fontSize); ?>">
-        <?php echo $data['text']; ?>
-
-    </p>
-</div>
